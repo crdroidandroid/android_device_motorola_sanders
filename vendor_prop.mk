e86ac852059964d827e17d06637206ef02eb5128
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-12-27 23:54:31 +0100

    sanders: Enable USB debugging at boot

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -347,3 +347,10 @@ persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
 persist.radio.data_lte_sys_ind=1
+
+# USB debugging at boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
