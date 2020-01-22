b816806e4a8afba4df91defe719185dc01206acf
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-01-22 18:47:28 -0300

    sanders: update some stuffs
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,5 +1,11 @@
 #!/sbin/sh
 
+# 1802 - Brazil
+# 1803 - Europe
+# 1804 - India
+# 1805 - Europe
+# 1806 - USA
+
 sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1806" ]; then
@@ -8,8 +14,20 @@ if [ "$sku" = "XT1806" ]; then
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm -r /system/app/NfcNci
+    rm -r /system/system/app/NfcNci
 else
     # Only XT1806 variant got a compass
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
+fi
+
+if ! [ "$sku" = "XT1802" ]; then
+    # Others variants doesn't have DTV support
+    rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /vendor/lib/libdtvtuner.so
+    rm /vendor/lib64/libdtvtuner.so
+    rm /vendor/lib/libdtvhal.so
+    rm /vendor/lib64/libdtvhal.so
+    rm -r /system/system/priv-app/DTVPlayer
+    rm -r /system/system/priv-app/DTVService
 fi
