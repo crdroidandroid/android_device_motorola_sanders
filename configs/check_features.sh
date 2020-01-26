3b4cd02d702b02900224901c2a73825d72fe9a50
Pranav Vashi <neobuddy89@gmail.com>
2020-01-26 16:08:08 +0530

    sanders: Clean up modelwise files properly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -8,30 +8,33 @@
 
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1806" ]; then
+if [ "$sku" == "XT1806" ]; then
     # XT1806 doesn't have NFC chip
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
-    rm -r /system/app/NfcNci
+    rm -rf /system/app/NfcNci
 fi
 
-if ! [ "$sku" = "XT1802" ]; then
+if [ "$sku" != "XT1802" ]; then
     # Others variants doesn't have DTV support
+    rm /system/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/etc/permissions/mot_dtv_permissions.xml
     rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
     rm /vendor/etc/permissions/mot_dtv_permissions.xml
     rm /vendor/lib/libdtvtuner.so
     rm /vendor/lib64/libdtvtuner.so
     rm /vendor/lib/libdtvhal.so
     rm /vendor/lib64/libdtvhal.so
-    rm -r /vendor/app/DTVPlayer
-    rm -r /vendor/app/DTVService
+    rm -rf /vendor/app/DTVPlayer
+    rm -rf /vendor/app/DTVService
 fi
 
-if [ "$sku" = "XT1804" ]; then
+if [ "$sku" == "XT1804" ]; then
     # XT1804 has additional thermal configs
+    rm -f /vendor/etc/thermal-engine.conf
     mv /vendor/etc/thermal-engine-INDIA.conf /vendor/etc/thermal-engine.conf
 else
-    rm /vendor/etc/thermal-engine-INDIA.conf
+    rm -f /vendor/etc/thermal-engine-INDIA.conf
 fi
 
