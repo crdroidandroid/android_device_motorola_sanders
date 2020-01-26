db4afef198cd75284dc8acb7125f0b1166cc2467
Pranav Vashi <neobuddy89@gmail.com>
2020-01-26 23:28:36 +0530

    sanders: Use aosp SurfaceFlinger
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -358,6 +358,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.glance_approach=false \
     ro.hardware.sensors=sanders
 
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.use_smart_90_for_video=true \
+    ro.surface_flinger.set_display_power_timer_ms=10000 \
+    ro.surface_flinger.set_touch_timer_ms=5000 \
+    ro.surface_flinger.set_idle_timer_ms=9000
+
 #Trim properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
