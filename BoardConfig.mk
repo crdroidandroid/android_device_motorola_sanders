1c821c77de5cd5e07177b5dffb939fd93c2357c3
Pranav Vashi <neobuddy89@gmail.com>
2020-06-19 18:40:35 +0530

    sanders: Selective dexpreopt
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,8 +167,15 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Enable dexpreopt to speed boot time
-WITH_DEXPREOPT := true
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 BOARD_USES_ADRENO := true
